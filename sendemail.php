b804a9db453859651cce1851b52ef84a00220ed5
Hanna <hberon00@gmail.com>
2023-10-26 22:20:15 -0300

    more php

--- sendemail.php
+++ sendemail.php
@@ -1,14 +1,26 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; // Load PHPMailer library
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = $_POST["to"];
     $subject = $_POST["subject"];
     $message = $_POST["message"];
-    $headers = "From: hberon00@gmail.com"; // Change this to your email address
 
-    if (mail($to, $subject, $message, $headers)) {
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->setFrom('hberon00@gmail.com'); // Change this to your email address
+        $mail->addAddress($to);
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        $mail->send();
         echo "Email sent successfully!";
-    } else {
-        echo "Email sending failed.";
+    } catch (Exception $e) {
+        echo "Email sending failed: " . $mail->ErrorInfo;
     }
 }
 ?>
