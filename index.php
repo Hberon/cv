6c44d19e551389e9e6b2853819a4e160b98bbe3f
Hanna <hberon00@gmail.com>
2023-10-26 22:04:19 -0300

    php stuff

---
+++ index.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta content="hanna bandidas" name="description">
+    <meta content="Hanna" name="author">
+    <meta content="las bandidas hanna store" name="keywords">
+    <link href="../../favicon.ico" rel="icon">
+
+    <title>YoMama</title><!-- Bootstrap core CSS -->
+    <link crossorigin="anonymous" href=
+    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    integrity=
+    "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+    rel="stylesheet"><!-- FontAwesome Icon Font -->
+    <link href=
+    "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
+    rel="stylesheet"><!-- LinearIcons Icon Font -->
+    <link href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css" rel=
+    "stylesheet"><!-- Main Stylesheet -->
+    <link href="css/style.css" rel="stylesheet"><!-- Timeline CSS -->
+    <link href="css/timeline.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- INTRO SECTION -->
+
+
+    <section class="section-image">
+        <div class="img-overlay">
+            <video playsinline autoplay muted loop>
+                <source src="videos/longbeachbrazil.mov" type="video/webm">
+              </video>
+        </div>
+            <div class="intro">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h5>Oi Putadas</h5>
+
+
+                        <h1>Esta e sua rainha!</h1>
+
+
+                        <p class="intro-desc">
+                            <span>Bailar</span>
+                            <span class="colour-splash">•</span>
+                            <span>Besar</span> 
+                            <span class="colour-splash">•</span>
+                            <span>Abrazar</span>
+                            <span class="colour-splash">•</span>
+                            <span>Emborrachar</span>
+                        </p>
+
+
+                    </div>
+                    <!-- /col-md-12 -->
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="social">
+                            <a href=
+                            "http://www.instagram.com/labandidabella" target=
+                            "_blank"><i class="fa fa-instagram fa-3x"></i></a> 
+                            <a href=
+                            "http://www.facebook.com/hannalamine.valentina" target=
+                            "_blank"><i class="fa fa-facebook fa-3x"></i></a> 
+                            <!-- <a href="http://www.dribbble.com/izzydoesizzy" target="_blank">
+                                  <i class="fa fa-dribbble fa-3x"></i>
+                                </a> -->
+                            <a href="https://ca.linkedin.com/in/hannaberon"
+                            target="_blank"><i class=
+                            "fa fa-linkedin fa-3x"></i></a>
+                            <a href=
+                            "http://www.github.com/Hberon" target=
+                            "_blank"><i class="fa fa-github fa-3x"></i></a>
+                        </div><!-- /social -->
+                    </div><!-- /col-md-8 -->
+                </div><!-- /row -->
+                
+
+
+                <div class="bottom">
+                    <a href="#about">
+                    <div class="mouse-icon">
+                        <div class="scroll">
+                        </div>
+                    </div></a>
+                </div>
+            </div><!-- /intro -->
+    </section>
+    
+    
+    <!-- ABOUT SECTION -->
+    <section class="section-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3 text-center">
+                    <img class="profilepic" src="images/beach.jpg"/>
+                    <h2>About Me</h2>
+
+                    <hr>
+
+
+                    <p class="lead">Life is a party and I'm the piñata.</p>
+                </div><!-- /col-md-6 -->
+            </div><!-- / row -->
+        </div><!-- /container -->
+    </section>
+    <!-- SKILLS SECTION -->
+
+
+    <section class="section-image" id="skills">
+        <div class="img-overlay"></div>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 section-heading">
+                    <h2>Hobbies</h2>
+
+
+                    <p class="lead">Here are a few cool things that I like to do in my free time</p>
+
+                    <hr>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <span class="lnr lnr-user"></span>
+
+                    <h2>Surfing</h2>
+
+
+                    <p>On the weekend I continue to wake up early, but for a great reason. I love carrying the heavy board into waves and it just takes all mental and physical strength.</p>
+                </div>
+
+
+                <div class="col-md-4 text-center">
+                    <span class="lnr lnr-star"></span>
+
+                    <h2>Dancing</h2>
+
+
+                    <p>Any and all kinds of dancing. Samba, Salsa, Cumbia, Bachata, Bhangra, Persian, Tahitian, Belly, Flamenco, Funky...whatever it is, I will like it. .</p>
+                </div>
+
+
+                <div class="col-md-4 text-center">
+                    <span class="lnr lnr-diamond"></span>
+
+                    <h2>Capoeira</h2>
+
+
+                    <p>For me, it's about the exercise and the community/culture surrounding this hobby.</p>
+                </div>
+            </div> <!-- / row -->
+        </div><!-- /container -->
+    </section>
+    <!-- TIMELINE SECTION -->
+
+
+    <section class="section-primary" id="timelinesection">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 section-heading">
+                    <h2>Things I've done</h2>
+
+
+                    <p class="lead">Here are a few cool things about me</p>
+
+                    <hr class="white">
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Timeline -->
+
+
+                    <div id="timeline">
+                        <!-- Timeline Item, copy from here to create various boxes -->
+
+
+                        <div class="timeline-item">
+                            <!--Icon inside the circle-->
+
+
+                            <div class="timeline-icon">
+                                <i class="fa fa-plane"></i>
+                            </div>
+                            <!-- Content from timeline box and position (right or left)-->
+
+
+                            <div class="timeline-content right">
+                                <h2>Visited Brazil</h2>
+
+
+                                <p>In August of 2023, I visited my father who has a new job in Salvador de Bahia, Braizil..</p>
+                            </div>
+                        </div>
+                        <!-- /Timeline Item-->
+                        <!-- Timeline Item, copy from here to create various boxes -->
+
+
+                        <div class="timeline-item">
+                            <!--Icon inside the circle-->
+
+
+                            <div class="timeline-icon">
+                                <i class="fa fa-plane"></i>
+                            </div>
+                            <!-- Content from timeline box and position (right or left)-->
+
+
+                            <div class="timeline-content left">
+                                <h2>Moved to Mexico</h2>
+
+
+                                <p>During the pandemic, I once again moved to Mexico. My sister joined me for a year and a half.</p>
+                            </div>
+                        </div>
+                        <!-- /Timeline Item-->
+                        <!-- Timeline Item, copy from here to create various boxes -->
+
+
+                        <div class="timeline-item">
+                            <!--Icon inside the circle-->
+
+
+                            <div class="timeline-icon">
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <!-- Content from timeline box and position (right or left)-->
+
+
+                            <div class="timeline-content right">
+                                <h2>Studied at the University of Haifa</h2>
+
+
+                                <p>In 2019 I moved to Haifa, Israel to do a masters degree in peace and conflict. .</p>
+                            </div>
+                        </div>
+                        <!-- /Timeline Item-->
+                        <!-- Timeline Item, copy from here to create various boxes -->
+
+
+                        <div class="timeline-item">
+                            <!--Icon inside the circle-->
+
+
+                            <div class="timeline-icon">
+                                <i class="fa fa-plane"></i>
+                            </div>
+                            <!-- Content from timeline box and position (right or left)-->
+
+
+                            <div class="timeline-content left">
+                                <h2>Spain Tour</h2>
+
+
+                                <p>In 2017 I went on a tour of Southern Spain with my Palestinian friend.</p>
+                            </div>
+                        </div>
+                        <!-- /Timeline Item-->
+                        <!-- Timeline Item, copy from here to create various boxes -->
+
+
+                        <div class="timeline-item">
+                            <!--Icon inside the circle-->
+
+
+                            <div class="timeline-icon">
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <!-- Content from timeline box and position (right or left)-->
+
+
+                            <div class="timeline-content right">
+                                <h2>Studied at University of North Carolina at Chapel Hill</h2>
+
+
+                                <p>In 2012 I transferred to this university .</p>
+                            </div>
+                        </div>
+                        <!-- /Timeline Item-->
+                    </div>
+                    <!-- /col-md-12 -->
+                </div>
+                <!-- /row -->
+            </div>
+        </div> <!-- / container -->
+    </section>
+    <!-- FOOTER SECTION -->
+
+<section>
+    <div class="container">
+        <h2>Send e-mail to hberon00@gmail.com:</h2>
+
+<form action="mailto:hberon00@gmail.com" method="post" enctype="text/plain">
+Name:<br>
+<input type="text" name="name"><br>
+E-mail:<br>
+<input type="text" name="mail"><br>
+Comment:<br>
+<input type="text" name="comment" size="50"><br><br>
+<input type="submit" value="Send">
+<input type="reset" value="Reset">
+</form>
+    </div>
+</section>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Developed by:<a href="http://www.github.com/Hberon" target=
+                    "_blank"> Hanna</a><br>
+                    <a href="http://creativecommons.org/licenses/by/4.0/"
+                    target="_blank"><i class="fa fa-creative-commons"></i>
+                    Attribution 4.0 License</a></p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-footer"> 
+                    <a href=
+                    "http://www.instagram.com/labandidabella" target=
+                    "_blank"><i class="fa fa-instagram"></i></a> 
+                    <a href=
+                    "http://www.facebook.com/hannalamine.valentina" target=
+                    "_blank"><i class="fa fa-facebook"></i></a> 
+                    <!-- <a href="http://www.dribbble.com/izzydoesizzy" target="_blank">
+                          <i class="fa fa-dribbble fa-3x"></i>
+                        </a> -->
+                    <a href="https://ca.linkedin.com/in/hannaberon"
+                    target="_blank"><i class=
+                    "fa fa-linkedin"></i></a>
+                    <a href=
+                    "http://www.github.com/Hberon" target=
+                    "_blank"><i class="fa fa-github"></i></a>
+                    </div> <!-- /social footer -->
+                </div> <!-- /col-md-4 -->
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </footer>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src=
+    "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
+    </script> 
+    <script>
+    window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')
+    </script> 
+    <script src="js/bootstrap.min.js">
+    </script> 
+    <script src="js/scripts.js">
+    </script>
+
+<?php
+
+?>
+
+</body>
+</html>
